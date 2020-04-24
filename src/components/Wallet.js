4faa67512043990152a4b319237528d11e3e9278
Alois Cochard <alois.cochard@gmail.com>
2020-04-24 11:28:17 +0200

    Add dialog to show transaction message.

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import TextField from '@material-ui/core/TextField';
 import { createClient } from "../utils/util";
 import ALF from "alf-client";
@@ -26,6 +31,9 @@ class Wallet extends Component {
     super();
     this.state = {
       address: '',
+      dialogOpen: false,
+      dialogTitle: '',
+      dialogMessage: '',
       privateKey: '',
       balance: 'unknown',
       transferTo: '',
@@ -37,7 +45,6 @@ class Wallet extends Component {
 
   render() {
     const { classes } = this.props;
-
     return (
       <div className={classes.root}>
         <div className={classes.form}>
@@ -75,6 +82,26 @@ class Wallet extends Component {
             <br/>
             <Button variant="contained" onClick={e => this.generateKeyPair()}>Generate</Button>
           </div>
+
+          <Dialog
+            open={this.state.dialogOpen}
+            onClose={this.dialogClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">{this.state.dialogTitle}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                {this.state.dialogMessage}
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={e => this.dialogClose()} color="primary">
+                Okay
+              </Button>
+            </DialogActions>
+          </Dialog>
+
         </div>
       </div>
     );
@@ -94,10 +121,20 @@ class Wallet extends Component {
   async transfer(e) {
     const response = await this.client.transfer(this.state.address, 'pkh', this.state.privateKey,
                                                 this.state.transferTo, 'pkh', this.state.transferValue);
-    alert('Transaction submitted\n' + 
-      '\tid: ' + response.result.txId + '\n' +
-      '\tchain index: ' + response.result.fromGroup + ' ➡ ' + response.result.toGroup);
+
+    this.setState({
+      dialogOpen: true,
+      dialogTitle: 'Transaction submitted',
+      dialogMessage: response.result.txId + '\n' +
+        'chain index: ' + response.result.fromGroup + ' ➡ ' + response.result.toGroup
+    });
   }
+
+  dialogClose() {
+    this.setState({
+      dialogOpen: false
+    });
+  };
 
   updateAddress(e) {
     this.setState({
