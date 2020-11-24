d1ba877ccd4f58d7a0be20eeb471e1875fcb7068
aloiscochard <alois.cochard@gmail.com>
2020-11-24 12:18:20 +0000

    Deploying to gh-pages from  @ 787f487fc492299e1295fa565fe53caad00560e1 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.181199d97b0e04c4e8581e6170c0feb2.js"
+  "/precache-manifest.59dc8ffc5bea0673c1d171fa6719847b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
