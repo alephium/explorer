5fdbf7f4e5a4e81f49e0084e9d638691002c6b71
Mikaël Vaivre <m.vaivre@gmail.com>
2021-06-04 13:53:31 +0200

    Improve display of small amounts

--- src/utils/util.tsx
+++ src/utils/util.tsx
@@ -40,7 +40,30 @@ export const truncateToDecimals = (num: number, dec = 2) => {
   return Math.trunc(num * calcDec) / calcDec
 }
 
+const removeTrailingZeros = (numString: string) => {
+  const numberArray = numString.split('')
+
+  let numberOfZeros = 0
+
+  for (let i = numberArray.length - 1; i >= 0; i--) {
+    if (numberArray[i] === '0') {
+      numberOfZeros++
+    } else {
+      break
+    }
+  }
+
+  const numberArrayWithoutTrailingZeros = [...numberArray.slice(0, numberArray.length - numberOfZeros)]
+
+  if (numberArrayWithoutTrailingZeros[numberArrayWithoutTrailingZeros.length - 1] === '.')
+    numberArrayWithoutTrailingZeros.push('0')
+
+  return numberArrayWithoutTrailingZeros.join().replaceAll(',', '')
+}
+
 export const abbreviateAmount = (baseNum: bigint, showFullPrecision = false, nbOfDecimals?: number) => {
+  const maxDecimals = 8
+
   if (baseNum < 0n) return '0.00'
 
   // For abbreviation, we don't need full precision and can work with number
@@ -53,11 +76,20 @@ export const abbreviateAmount = (baseNum: bigint, showFullPrecision = false, nbO
   const numberOfNonZero =
     numberArray.length - numberArray.reduceRight<number>((a, v) => Number(a) + (Number(v) === 0 ? 1 : 0), 0)
 
-  const numberOfDigitsToDisplay = nbOfDecimals ? nbOfDecimals : numberOfNonZero < 4 ? numberOfNonZero : 4
+  const numberOfDigitsToDisplay = nbOfDecimals
+    ? nbOfDecimals
+    : numberOfNonZero < maxDecimals
+    ? numberOfNonZero
+    : maxDecimals
 
-  // if zero, we don't need a suffix
-  if (tier <= 0) return alephNum.toFixed(numberOfDigitsToDisplay).toString() // Small number, low precision is ok
-  if (tier >= MONEY_SYMBOL.length) tier = MONEY_SYMBOL.length - 1
+  if (tier < 0) {
+    return removeTrailingZeros(alephNum.toFixed(8))
+  } else if (tier === 0) {
+    // Small number, low precision is ok
+    return removeTrailingZeros(alephNum.toFixed(numberOfDigitsToDisplay).toString())
+  } else if (tier >= MONEY_SYMBOL.length) {
+    tier = MONEY_SYMBOL.length - 1
+  }
 
   // get suffix and determine scale
   const suffix = MONEY_SYMBOL[tier]
@@ -71,8 +103,7 @@ export const abbreviateAmount = (baseNum: bigint, showFullPrecision = false, nbO
     // Work with string to avoid rounding issues
     const nonDigitLength = Math.round(scaled).toString().length
     const numberArrayWithDecimals = [...numberArray.slice(0, nonDigitLength), '.', ...numberArray.slice(nonDigitLength)]
-    const numberArrayWithoutTrailingZeros = [...numberArrayWithDecimals.slice(0, numberOfNonZero + 2)] // Taking into account the dot
-    return numberArrayWithoutTrailingZeros.join().replaceAll(',', '') + suffix
+    return removeTrailingZeros(numberArrayWithDecimals.join().replaceAll(',', '')) + suffix
   }
 
   return scaled.toFixed(numberOfDigitsToDisplay) + suffix
