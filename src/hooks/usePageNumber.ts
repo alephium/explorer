1ac9cb960aac7969057f1ef9788664e8defdb5e6
Mikaël Vaivre <m.vaivre@gmail.com>
2021-07-14 19:48:22 +0200

    Don't append ?p=1 by default, only add query param when switching page

--- src/hooks/usePageNumber.ts
+++ src/hooks/usePageNumber.ts
@@ -1,17 +1,8 @@
-import { useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
 import useQueryParams from './useQueryParams'
 
 const usePageNumber = () => {
-  const history = useHistory()
-  const location = history.location
   const pageParam = useQueryParams('p')
   const pageNumber = pageParam && parseInt(pageParam)
-
-  // Default page query param
-  useEffect(() => {
-    if (!pageParam) history.replace(location.pathname + '?p=1')
-  }, [history, location.pathname, pageParam])
 
   return pageNumber || 1
 }
