9fdf1e84fa3b17dbb096e3777d93c5febdfb4f9b
Mikaël Vaivre <m.vaivre@gmail.com>
2021-07-15 14:11:28 +0200

    Remove page query param when p=1

--- src/hooks/usePageNumber.ts
+++ src/hooks/usePageNumber.ts
@@ -1,8 +1,19 @@
+import { useMemo } from 'react'
+import { useHistory } from 'react-router-dom'
 import useQueryParams from './useQueryParams'
 
 const usePageNumber = () => {
   const pageParam = useQueryParams('p')
   const pageNumber = pageParam && parseInt(pageParam)
+  const history = useHistory()
+  const location = history.location
+
+  const locationSearch = useMemo(() => new URLSearchParams(location.search), [location.search])
+
+  if (pageNumber === 1) {
+    locationSearch.delete('p')
+    history.push({ search: locationSearch.toString() })
+  }
 
   return pageNumber || 1
 }
