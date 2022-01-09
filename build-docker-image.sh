d70b165541e520512dbffa53241259f9da0e1fc5
Hongchao Liu <lbs.lhc@gmail.com>
2022-01-09 10:26:21 +0100

    Add environment variables to Dockerfile

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -5,7 +5,11 @@ CURRENT_COMMIT=`git rev-parse HEAD`
 CURRENT_TAG=`git tag --points-at HEAD`
 IMAGE_NAME=alephium/explorer:${CURRENT_COMMIT}
 
-docker build -t $IMAGE_NAME $DIR
+docker build \
+       --build-arg backend_url=${REACT_APP_BACKEND_URL:-http://localhost:9090} \
+       --build-arg network_type=${REACT_APP_NETWORK_TYPE:-mainnet} \
+       -t $IMAGE_NAME \
+       $DIR
 
 if [ ! -z $CURRENT_TAG ]
 then
