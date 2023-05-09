ac89f0f61f5925808490bcb7863f56b972d7ed5e
Ilias Trichopoulos <hlias.nop@gmail.com>
2023-05-09 15:35:13 +0200

    Upgrade React to 18

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { FC, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import styled, { css, DefaultTheme, FlattenInterpolation, ThemeProps } from 'styled-components'
 
 import SkeletonLoader from '@/components/SkeletonLoader'
--- src/pages/BlockInfoPage.tsx
+++ src/pages/BlockInfoPage.tsx
@@ -20,7 +20,7 @@ import { APIError } from '@alephium/sdk'
 import { AssetOutput, BlockEntryLite, Transaction } from '@alephium/sdk/api/explorer'
 import { ALPH } from '@alephium/token-list'
 import { ArrowRight } from 'lucide-react'
-import { FC, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import styled, { css } from 'styled-components'
 
